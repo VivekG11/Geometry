b7b7a60b08986a6af1c83a4493d75d72d74f0da7
VivekG11 <vivekvk2903@gmail.com>
2021-06-28 22:01:29 +0530

    Length of the line

---
+++ Geometry/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Geometry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the values of co-oridinates");
+            int x1 = Convert.ToInt32( Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double Length_of_Line = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of the line is : " + Length_of_Line);
+            Console.Read();
+
+
+        }
+    }
+}
