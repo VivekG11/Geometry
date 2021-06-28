ca3d7f1681189bf4f448cc441812d5a4d820de90
VivekG11 <vivekvk2903@gmail.com>
2021-06-28 23:05:17 +0530

    Comparing using Switch statement

--- Geometry/Program.cs
+++ Geometry/Program.cs
@@ -20,7 +20,7 @@ namespace Geometry
             Console.WriteLine("Length of the line is : " + Length_of_Line);
             double Length_of_Line_Two = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
             Console.WriteLine("Length of the line is : " + Length_of_Line_Two);
-            Console.Read();
+            
 
             if (Length_of_Line.Equals(Length_of_Line_Two))
             {
@@ -30,6 +30,21 @@ namespace Geometry
             {
                 Console.WriteLine("Not Equal");
             }
+            Console.WriteLine("Comparing two Lines ");
+
+            switch(Length_of_Line .CompareTo(Length_of_Line_Two))
+            {
+                case 1:
+                    Console.WriteLine("First line is greater than Second Line");
+                    break;
+                case 2:
+                    Console.WriteLine("Second Line is Greater then First Line");
+                    break;
+                default:
+                    Console.WriteLine("Both Lines Re Equal");
+                    break;
+            }
+            Console.Read();
         }
     }
 }
